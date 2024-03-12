4d829ad5ea1bba42a54c6a43c6dde1e0589fc144
Ilya <titov.ie@phystech.edu>
2024-03-12 22:11:34 +0300

    init commit

---
+++ src/main/java/ru/otus/databasefiltermod/DatabaseFilterModApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.databasefiltermod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseFilterModApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseFilterModApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/otus/databasefiltermod/DatabaseFilterModApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.databasefiltermod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseFilterModApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
